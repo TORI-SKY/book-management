025ce95678b8e0224770dabbd9faccadeff5d539
Ny-Yanty <nyyanty117@gmail.com>
2024-07-15 16:26:51 +0700

    logo changed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,17 +2,23 @@ import React from "react";
 import Profile from "./Profile";
 import Link from "next/link";
 import MenuLinks from "./MenuLinks";
+import Image from 'next/image'
 
 export default function Navbar() {
 	return (
 		<div className="flex justify-between items-center h-20 ">
 			<div className="flex space-x-10">
-			<Link href="/" className="my-auto">
-				<h1 className="text-xl font-bold">SPRINGBOARD4EDUCATION</h1>
-			</Link>
-			<MenuLinks />
+				<Link href="/" className="my-auto">
+					<Image
+						src="/logo.png"
+						width={300}
+						height={200}
+						alt="springbaord logo"
+					/>
+				</Link>
+				<MenuLinks />
 			</div>
-			
+
 			<Profile />
 		</div>
 	);
